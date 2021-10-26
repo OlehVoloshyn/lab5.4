764b034ad6aa248b7ee706d3aa665ec08e90ec9d
Олег Волошин <olegvolosin922@gmail.com>
2021-10-26 23:09:06 +0300

    lab5.4

---
+++ UnitTest5.4/UnitTest5.4.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab5.4/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest54
+{
+	TEST_CLASS(UnitTest54)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			int i{};
+			t = S0(0,5);
+			Assert::AreEqual(t, (1. / i) + (sqrt(1 + sin(1. * i) * sin(1. * i))));
+		}
+	};
+}
